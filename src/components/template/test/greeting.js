6eb3bcbd70648e7ad91960d7d0caa47f478405af
stradava <32715015+stradava@users.noreply.github.com>
2023-07-07 10:25:12 +0700

    changed root div to have 100vh height

--- src/components/template/test/greeting.js
+++ src/components/template/test/greeting.js
@@ -16,6 +16,21 @@ export const Greeting = () => {
     return (
         <div className="overlay justify-content-center">
             <div className="home-container">
+                <div className="row">
+                    <h5>
+                        <p>- There are 50 multiple choices test problems.</p>
+                    </h5>
+                </div>
+                <div className="row">
+                    <h5>
+                        <p>- You have 1 hour to finish the test.</p>
+                    </h5>
+                </div>
+                <div className="row">
+                    <h5>
+                        <p>- You are allowed to leave mid test and come back later, but the timer will keep running!.</p>
+                    </h5>
+                </div>
                 <button onClick={() => startTest()} className="btn btn-primary text-center start-button">
                     Start Test
                 </button>
